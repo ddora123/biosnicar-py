1e408577ed39f13665a3490bf5ea8ea93e88bad2
ddora123 <dora@ibi.one>
2025-05-14 09:33:06 +0200

    changes in albedo model mat

--- biosnicar/drivers/get_albedo.py
+++ biosnicar/drivers/get_albedo.py
@@ -52,3 +52,36 @@ def get(solver, plot, validate):
         plot_albedo(plot_config, model_config, outputs.albedo)
     display_out_data(outputs)
     return outputs
+
+
+def get_grid(solver, ice, illumination, rt_config, model_config, plot_config, impurities, plot,validate):
+    if validate:
+        validate_inputs(ice, illumination, impurities)
+
+    # now get the optical properties of the ice column
+    ssa_snw, g_snw, mac_snw = get_layer_OPs(ice, model_config)
+    tau, ssa, g, L_snw = mix_in_impurities(
+        ssa_snw, g_snw, mac_snw, ice, impurities, model_config
+    )
+
+    # now run one or both of the radiative transfer solvers
+    if solver == "toon":
+        #print("\nRunning biosnicar with the Toon solver\n")
+        outputs = toon_solver(
+            tau, ssa, g, L_snw, ice, illumination, model_config, rt_config
+        )
+    elif solver == "adding-doubling":
+        #print("\nRunning biosnicar with the adding-doubling solver\n")
+
+        outputs = adding_doubling_solver(
+            tau, ssa, g, L_snw, ice, illumination, model_config
+        )
+    else:
+        raise ValueError(
+            "ERROR: solver not recognized, please choose toon or adding-doubling"
+        )
+        return
+    plot=False    
+    if plot:
+        plot_albedo(plot_config, model_config, outputs.albedo)
+    return outputs
